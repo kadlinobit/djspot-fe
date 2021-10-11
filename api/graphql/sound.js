0bf80b83dcedb6d3457775a0f36ffd296c2cda8d
Jan Kadlec <kadlec@cngroup.dk>
2021-10-11 10:44:42 +0200

    Refactoring sound graphql

--- api/graphql/sound.js
+++ api/graphql/sound.js
@@ -1,25 +1,145 @@
 import { gql } from 'graphql-tag'
 
-export const getSounds = function () {
+const fragments = {
+    default: `
+        id
+        created_at
+        updated_at
+        name
+        description
+        url
+        dj {
+            id
+            name
+            slug
+            city  
+        }
+        type
+        duration
+        photo {
+            url
+            previewUrl
+            formats
+        }
+        published_at
+        genres {
+            id,
+            name
+        }
+        likesCount
+        isLikedByMe
+        `,
+    detailed: `
+        id
+        created_at
+        updated_at
+        name
+        description
+        url
+        dj {
+            id
+            name
+            slug
+            city  
+        }
+        type
+        duration
+        photo {
+            url
+            previewUrl
+            formats
+        }
+        published_at
+        genres {
+            id,
+            name
+        }
+        likesCount
+        isLikedByMe
+    `,
+    form: `
+        id
+        name
+        description
+        url
+        dj {
+            id
+        }
+        type
+        duration
+        genres {
+            id
+            name
+        }
+        photo {
+            id
+            url
+        }
+    `,
+    list: `
+        id
+        created_at
+        updated_at
+        name
+        description
+        url
+        dj {
+            id
+            name
+            slug
+        }
+        type
+        duration
+        genres {
+            id
+            name
+        }
+        photo {
+            id
+            url
+            formats
+        }
+    `
+}
+
+export const getSound = function (fragmentName) {
+    const query = gql`
+        query ($id: ID!) {
+            sound(id: $id) {
+                ${
+                    fragmentName && fragments[fragmentName]
+                        ? fragments[fragmentName]
+                        : fragments.default
+                }
+            }
+        }
+    `
+    return query.loc.source.body
+}
+
+export const getSounds = function (fragmentName) {
     const query = gql`
         query ($where: JSON) {
             sounds(where: $where) {
-                id
-                created_at
-                updated_at
-                name
-                description
-                url
-                dj {
-                    id
-                    name
-                    slug
+                ${
+                    fragmentName && fragments[fragmentName]
+                        ? fragments[fragmentName]
+                        : fragments.default
                 }
-                type
-                duration
-                genres {
-                    id
-                    name
+            }
+        }
+    `
+    return query.loc.source.body
+}
+
+export const toggleSoundLike = function (fragmentName) {
+    const query = gql`
+        mutation ($id: ID!) {
+            toggleSoundLike(id: $id) {
+                ${
+                    fragmentName && fragments[fragmentName]
+                        ? fragments[fragmentName]
+                        : fragments.default
                 }
             }
         }
