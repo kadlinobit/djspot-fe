71742583d336f26fa5cbc6eae19164df632847f1
Jan Kadlec <kadlec@cngroup.dk>
2021-11-08 19:53:19 +0100

    Creating dj control box

---
+++ components/dj/DjControlBox.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="dj-control-box level is-mobile">
+        <div class="level-left">
+            <div class="level-item">
+                <b-responsive-button
+                    icon-left="eye"
+                    :type="dj.isFollowedByMe ? 'is-dark' : 'is-light'"
+                    @click="onToggleFollow"
+                >
+                    {{
+                        dj.followsCount > 0
+                            ? `${dj.followsCount} ${$t('dj.followers')}`
+                            : $t('dj.follow')
+                    }}
+                </b-responsive-button>
+            </div>
+        </div>
+        <div class="level-right">
+            <b-responsive-button
+                type="is-dark"
+                icon-left="pencil"
+                tag="nuxt-link"
+                :to="{ path: `/djs/manage/edit/` }"
+            >
+                {{ $t('form.edit') }}
+            </b-responsive-button>
+        </div>
+    </div>
+</template>
+
+<script>
+import BResponsiveButton from '~/components/form/BResponsiveButton.vue'
+export default {
+    components: {
+        BResponsiveButton
+    },
+    props: {
+        dj: {
+            type: Object,
+            required: true
+        },
+        onToggleFollow: {
+            type: Function,
+            required: true
+        }
+    }
+}
+</script>
