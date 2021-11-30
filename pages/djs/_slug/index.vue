a4f666a5426de20683e3cb85ecbfeb5a90fed55b
Jan Kadlec <kadlec@cngroup.dk>
2021-11-30 10:51:56 +0100

    Updating dj profile page with hero

--- pages/djs/_slug/index.vue
+++ pages/djs/_slug/index.vue
@@ -1,83 +1,84 @@
 <template>
-    <section class="section">
-        <div class="container is-max-desktop">
-            <o-loading
-                v-if="$fetchState.pending"
-                :full-page="false"
-                :active.sync="$fetchState.pending"
-                :can-cancel="true"
-            />
-            <p v-else-if="$fetchState.error">{{ $fetchState.error.message }}</p>
-            <div v-else>
-                <div class="columns is-mobile">
-                    <div class="column">
-                        <div class="columns is-mobile is-vcentered">
-                            <div class="column">
-                                <h1 class="title is-size-3-mobile is-size-2-desktop">
-                                    {{ dj.name }}
-                                </h1>
-                                <h2 class="subtitle">{{ dj.city }}</h2>
+    <o-loading
+        v-if="$fetchState.pending"
+        :full-page="false"
+        :active.sync="$fetchState.pending"
+        :can-cancel="true"
+    />
+    <p v-else-if="$fetchState.error">{{ $fetchState.error.message }}</p>
+    <div v-else>
+        <section class="hero is-secondary">
+            <div class="hero-body">
+                <div class="container is-max-desktop">
+                    <div class="columns is-mobile">
+                        <div class="column">
+                            <div class="columns is-mobile is-vcentered">
+                                <div class="column">
+                                    <h1 class="title is-size-3-mobile is-size-2-desktop">
+                                        {{ dj.name }}
+                                    </h1>
+                                    <h2 class="subtitle">{{ dj.city }}</h2>
+                                </div>
+                                <div class="column is-hidden-tablet is-narrow">
+                                    <cover-image
+                                        quality="small"
+                                        cover-type="dj"
+                                        :pixel-size="100"
+                                        :cover-image="dj.photo || null"
+                                    />
+                                </div>
                             </div>
-                            <div class="column is-hidden-tablet is-narrow">
-                                <cover-image
-                                    quality="small"
-                                    cover-type="dj"
-                                    :pixel-size="100"
-                                    :cover-image="dj.photo || null"
-                                />
+                            <div class="tags">
+                                <span
+                                    v-for="genre in dj.genres"
+                                    :key="`genre-${genre.id}`"
+                                    class="
+                                        tag
+                                        is-peach is-size-5-desktop is-size-6-mobile is-size-6-tablet
+                                    "
+                                >
+                                    {{ genre.name }}
+                                </span>
                             </div>
                         </div>
-                        <div class="tags">
-                            <span
-                                v-for="genre in dj.genres"
-                                :key="`genre-${genre.id}`"
-                                class="
-                                    tag
-                                    is-peach is-size-5-desktop is-size-6-mobile is-size-6-tablet
-                                "
-                            >
-                                {{ genre.name }}
-                            </span>
-                        </div>
-                    </div>
-                    <div class="column is-hidden-mobile is-narrow">
-                        <cover-image
-                            quality="small"
-                            cover-type="dj"
-                            :pixel-size="300"
-                            :cover-image="dj.photo || null"
-                        />
-                    </div>
-                </div>
-                <dj-control-box :dj="dj" :on-toggle-follow="onToggleFollow" />
-                <section>
-                    <o-tabs
-                        v-if="
-                            dj.bio || (mixes && mixes.length > 0) || (tracks && tracks.length > 0)
-                        "
-                        v-model="activeTab"
-                        :expanded="true"
-                        :animated="true"
-                    >
-                        <o-tab-item v-if="!!dj.bio" label="Bio">
-                            <div
-                                v-dompurify-html="$marked.markdownToHtml(dj.bio)"
-                                class="content"
+                        <div class="column is-hidden-mobile is-narrow">
+                            <cover-image
+                                quality="small"
+                                cover-type="dj"
+                                :pixel-size="300"
+                                :cover-image="dj.photo || null"
                             />
-                        </o-tab-item>
+                        </div>
+                    </div>
 
-                        <o-tab-item v-if="mixes && mixes.length > 0" label="Sety">
-                            <SoundList :sounds="mixes" />
-                        </o-tab-item>
-
-                        <o-tab-item v-if="tracks && tracks.length > 0" label="Tracky">
-                            <SoundList :sounds="tracks" />
-                        </o-tab-item>
-                    </o-tabs>
-                </section>
+                    <dj-control-box :dj="dj" :on-toggle-follow="onToggleFollow" />
+                </div>
             </div>
-        </div>
-    </section>
+        </section>
+
+        <section class="section p-3">
+            <div class="container is-max-desktop">
+                <o-tabs
+                    v-if="dj.bio || (mixes && mixes.length > 0) || (tracks && tracks.length > 0)"
+                    v-model="activeTab"
+                    :expanded="true"
+                    :animated="true"
+                >
+                    <o-tab-item v-if="!!dj.bio" label="Bio">
+                        <div v-dompurify-html="$marked.markdownToHtml(dj.bio)" class="content" />
+                    </o-tab-item>
+
+                    <o-tab-item v-if="mixes && mixes.length > 0" label="Sety">
+                        <SoundList :sounds="mixes" />
+                    </o-tab-item>
+
+                    <o-tab-item v-if="tracks && tracks.length > 0" label="Tracky">
+                        <SoundList :sounds="tracks" />
+                    </o-tab-item>
+                </o-tabs>
+            </div>
+        </section>
+    </div>
 </template>
 
 <script>
