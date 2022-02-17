bca7e056d6e5fdc63647d95fa98685db4798a44e
Jan Kadlec <kadlec@cngroup.dk>
2022-02-17 14:55:51 +0100

    Migrating reset password to directus

--- pages/reset-password.vue
+++ pages/reset-password.vue
@@ -1,58 +1,60 @@
 <template>
-    <section class="section">
-        <div class="container">
-            <div class="columns">
-                <div class="column is-4 is-offset-4">
-                    <h2 class="title has-text-centered">{{ $t('user.reset_password') }}</h2>
+    <client-only>
+        <section class="section">
+            <div class="container">
+                <div class="columns">
+                    <div class="column is-4 is-offset-4">
+                        <h2 class="title has-text-centered">{{ $t('user.reset_password') }}</h2>
 
-                    <o-notification v-if="success" variant="success" :closable="false">
-                        {{ $t(success) }}
-                    </o-notification>
-                    <o-notification v-if="error" variant="danger" :closable="false">
-                        {{ $t(error) }}
-                    </o-notification>
-                    <ValidationObserver ref="observer">
-                        <form v-if="!success" method="post" @submit.prevent>
-                            <o-validated-field
-                                v-model="password1"
-                                vid="password1"
-                                name="password1"
-                                type="password"
-                                :label="$t('user.new_password')"
-                                rules="required"
-                            />
+                        <o-notification v-if="success" variant="success" :closable="false">
+                            {{ $t(success) }}
+                        </o-notification>
+                        <o-notification v-if="error" variant="danger" :closable="false">
+                            {{ $t(error) }}
+                        </o-notification>
+                        <ValidationObserver ref="observer">
+                            <form v-if="!success" method="post" @submit.prevent>
+                                <o-validated-field
+                                    v-model="password1"
+                                    vid="password1"
+                                    name="password1"
+                                    type="password"
+                                    :label="$t('user.new_password')"
+                                    rules="required"
+                                />
 
-                            <o-validated-field
-                                v-model="password2"
-                                name="password2"
-                                type="password"
-                                :label="$t('user.password_again')"
-                                rules="required|confirmed:password1"
-                            />
-                            <div class="field">
-                                <div class="control">
-                                    <o-button
-                                        :disabled="isLoading"
-                                        variant="dark is-fullwidth"
-                                        @click="onSubmit"
-                                    >
-                                        {{ $t('user.do_reset_password') }}
-                                    </o-button>
+                                <o-validated-field
+                                    v-model="password2"
+                                    name="password2"
+                                    type="password"
+                                    :label="$t('user.password_again')"
+                                    rules="required|confirmed:password1"
+                                />
+                                <div class="field">
+                                    <div class="control">
+                                        <o-button
+                                            :disabled="isLoading"
+                                            variant="dark is-fullwidth"
+                                            @click="onSubmit"
+                                        >
+                                            {{ $t('user.do_reset_password') }}
+                                        </o-button>
+                                    </div>
                                 </div>
-                            </div>
-                        </form>
-                    </ValidationObserver>
-                    <div v-if="success" class="has-text-centered" style="margin-top: 20px">
-                        <p>
-                            <nuxt-link :to="{ path: '/login' }">
-                                {{ $t('user.go_to_login_page') }}
-                            </nuxt-link>
-                        </p>
+                            </form>
+                        </ValidationObserver>
+                        <div v-if="success" class="has-text-centered" style="margin-top: 20px">
+                            <p>
+                                <nuxt-link :to="{ path: '/login' }">
+                                    {{ $t('user.go_to_login_page') }}
+                                </nuxt-link>
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
+    </client-only>
 </template>
 
 <script>
@@ -70,22 +72,21 @@ export default {
     },
     plugins: ['vee-validate'],
     middleware: 'guest',
-    asyncData(context) {
-        if (!context.route.query.code) {
-            context.redirect('/forgot-password')
-        } else {
-            return {
-                code: context.route.query.code
-            }
-        }
-    },
     data() {
         return {
             password1: '',
             password2: '',
             success: null,
             error: null,
-            isLoading: false
+            isLoading: false,
+            token: null
+        }
+    },
+    fetch() {
+        if (!this.$nuxt.context.route.query.token) {
+            this.$router.push('/forgot-password')
+        } else {
+            this.token = this.$nuxt.context.route.query.token
         }
     },
     methods: {
@@ -106,26 +107,14 @@ export default {
             try {
                 this.isLoading = true
 
-                /* AUTH METHOD
-                await this.$axios.post('auth/reset-password', {
-                    code: this.code,
-                    password: this.password1,
-                    passwordConfirmation: this.password2
-                }) */
-
-                await this.$strapi.resetPassword({
-                    code: this.$route.query.code,
-                    password: this.password1,
-                    passwordConfirmation: this.password2
+                await this.$axios.post('auth/password/reset', {
+                    token: this.token,
+                    password: this.password1
                 })
 
                 this.success = 'user.password_reset_success'
             } catch (e) {
-                if (e.response && e.response.data) {
-                    this.error = e.response.data.message[0].messages[0].message
-                } else {
-                    this.error = e
-                }
+                this.error = this.$api.tools.parseResponseErrorMessage(e)
             } finally {
                 this.isLoading = false
             }
