d5e27aa6c9e5a2e2f6ae0a921aebc1c020daf1dc
Jan Kadlec <kadlec@cngroup.dk>
2026-05-12 11:53:56 +0200

    genre cloud

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,14 +1,19 @@
 <template>
     <UContainer class="py-10">
         <h1 class="title">{{ $i18n.t('welcome_message') }}</h1>
-        <p v-if="isLoggedIn">{{ remoteUserData }}</p>
-        <p v-else>User is not logged in!!!!</p>
+        <!-- <p v-if="isLoggedIn">{{ remoteUserData }}</p>
+        <p v-else>User is not logged in!!!!</p> -->
+
+        <div class="mt-10">
+            <GenresCloud />
+        </div>
     </UContainer>
 </template>
 
 <script setup lang="ts">
 import { useUserStore } from '#imports';
 import { readMe } from '@directus/sdk';
+import GenresCloud from '~/components/genre/GenresCloud.vue';
 const { $i18n, $directus } = useNuxtApp();
 
 const { isLoggedIn } = useUserStore();
